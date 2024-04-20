04b9f22b66973ed76aea0925255d5b73ce06393e
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-20 16:40:16 +0200

    add: working milestone 2 with cleaning of imput

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-      todos: [
-        { id: 1, text: "Recruiting blog post", done: true },
-        { id: 2, text: "Mobile app launch", done: true },
-        { id: 3, text: "Interview John H.", done: false },
-        { id: 4, text: "Summit update to mobile storefronts", done: true }
-      ]
-    };
-  }
+    data() {
+        return {
+            todos: [
+                { id: 1, text: "Recruiting blog post", done: true },
+                { id: 2, text: "Mobile app launch", done: true },
+                { id: 3, text: "Interview John H.", done: false },
+                { id: 4, text: "Summit update to mobile storefronts", done: true }
+            ],
+            newTodoText: ""
+        };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodoText !== "") {
+                const newTodo = {
+                    id: this.todos.length + 1,
+                    text: this.newTodoText,
+                    done: false
+                };
+                this.todos.push(newTodo);
+                // pulizia
+                this.newTodoText = "";
+            }
+        }
+    }
 }).mount('#app');
